6e7d99aa512e935c140a4cf2c170824d4dc28171
Ben Smith <ben.smith.mail14@gmail.com>
2019-02-08 18:38:16 +0000

    Some of the query code has been written for the bids table

--- myBids.php
+++ myBids.php
@@ -5,34 +5,57 @@
 <html>
 
 <body>
-    <?php 
-        $query = 'SELECT  FROM ';
-        echo '<table border="0" cellspacing="2" cellpadding="2"> 
+    <?php
+        $userID = 9;
+        $query = $pdo->prepare("SELECT DISTINCT(auctionID) FROM bids WHERE userID = ?");
+        $query->execute(array($userID));
+        $auctionArray = $query->fetchall();
+
+        $rowData = array();
+        if ($auctionArray) {
+            $query = $pdo->prepare("SELECT i.itemName, i.description, MAX(b.bidAmount) AS yourBid, b.bidDatetime AS yourBiddt, a.endDatetime
+                                   FROM items AS i, auctions AS a, bids AS b
+                                   WHERE b.userID = ?
+                                   AND b.auctionID = ?
+                                   AND i.itemID = a.itemID
+                                   AND a.auctionID = b.auctionID");
+
+            foreach ($auctionArray as $auctionID) {
+                $query->execute(array($userID, $auctionID["auctionID"]));
+                $returnedRow = $query->fetch();
+                $rowData = array_push($rowData, $returnedRow);
+            }
+
+            $query = $pdo->prepare("SELECT MAX(bidAmount) AS highestBid, bidDatetime AS highestBiddt FROM bids
+                                    WHERE auctionID = ?");
+
+            for ($i = 0; $i < count($auctionArray); $i++) {
+                $query->execute(array($auctionArray[$i]));
+                $rowData[$i] = array_merge($rowData[$i], $query->fetch());
+            }
+
+            echo '<table border="0" cellspacing="2" cellpadding="2"> 
             <tr> 
-                <td> <font face="Arial">Value1</font> </td> 
-                <td> <font face="Arial">Value2</font> </td> 
-                <td> <font face="Arial">Value3</font> </td> 
-                <td> <font face="Arial">Value4</font> </td> 
-                <td> <font face="Arial">Value5</font> </td> 
+                <td> <font face="Arial">Item Name</font> </td> 
+                <td> <font face="Arial">Item Description</font> </td> 
+                <td> <font face="Arial">Your Latest Bid</font> </td> 
+                <td> <font face="Arial">Highest Bid</font> </td> 
+                <td> <font face="Arial">End Date</font> </td> 
             </tr>';
-        if ($result = $mysqli->query($query)) {
-            while ($row = $result->fetch_assoc()) {
-                $field1name = $row["col1"];
-                $field2name = $row["col2"];
-                $field3name = $row["col3"];
-                $field4name = $row["col4"];
-                $field5name = $row["col5"]; 
-         
+
+            foreach ($rowData as $row) {         
                 echo '<tr> 
-                          <td>'.$field1name.'</td> 
-                          <td>'.$field2name.'</td> 
-                          <td>'.$field3name.'</td> 
-                          <td>'.$field4name.'</td> 
-                          <td>'.$field5name.'</td> 
+                          <td>'.$row["itemName"].'</td> 
+                          <td>'.$row["description"].'</td> 
+                          <td>'.$row["yourBid"] . $row["yourBiddt"].'</td> 
+                          <td>'.$row["highestBid"] . $row["highestBiddt"].'</td> 
+                          <td>'.$row["endDatetime"].'</td> 
                       </tr>';
             }
             $result->free();
-        } 
+        } else {
+            echo '<h1>No bids made<h1>';
+        }   
     ?>
 </body>
 
