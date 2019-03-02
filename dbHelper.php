a5562981206031cbd393f7bd0b5daf1950da2f25
shanpandya <a@as-MacBook-Pro.local>
2019-03-02 13:47:54 +0000

    add phpmailer to main directory

--- dbHelper.php
+++ dbHelper.php
@@ -2,9 +2,9 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require '../PHPMailer/src/Exception.php';
-require '../PHPMailer/src/PHPMailer.php';
-require '../PHPMailer/src/SMTP.php';
+require './PHPMailer/src/Exception.php';
+require './PHPMailer/src/PHPMailer.php';
+require './PHPMailer/src/SMTP.php';
 if(!defined("accessChecker")) {
     die("Direct access not permitted");
 }
