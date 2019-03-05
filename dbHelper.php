a0d8936cb09be11b39c372a1e97920c9fdce58da
shanpandya <a@as-MacBook-Pro.local>
2019-03-05 15:02:46 +0000

    added function to email seller after an auction has ended

--- dbHelper.php
+++ dbHelper.php
@@ -500,7 +500,7 @@ class DBHelper
         ob_start();
         $query->execute(array($auctionID));
         $row = $query->fetch();
-        if (isset($row)) {
+        if (!isset($row)) {
             return;
         }
         $userID = $row['userID'];
@@ -595,7 +595,93 @@ class DBHelper
         }
     }
 
-    // public function sendEmailToSellerAtAuctionEnd
+    public function sendEmailToSellerAtAuctionEnd ($auctionID) {
+        $query = $this->dbconnection->prepare(
+            "SELECT username, email, userID
+            FROM users, items, auctions, bids
+            WHERE auctions.auctionID = ?
+            AND auctions.itemID = items.itemID
+            AND items.sellerID = users.userID
+            AND auctions.auctionID = bids.auctionID
+            LIMIT 1"
+        );
+        ob_start();
+        $query->execute(array($auctionID));
+        $row = $query->fetch();
+        $userID = $row['userID'];
+        $username = $row['username'];
+        $email = $row['email'];
+
+        if (isset($row)) {
+            $subj = 'Congratulations, your item Sold!';
+            $msg = '<p>Hi ' . $username . '</p><p>Your item sold! Auction ID: ' . $auctionID . '</p><p> Please go to http://localhost:8888/itemAuction.php?auctionID=' . $auctionID . ' to see your sale</p>';
+        }
+        else{
+            $subj = 'Commiserations, your item didn\'t sell';
+            $msg = '<p>Hi ' . $username . '</p><p>Your item didn\'t sell. Auction ID: ' . $auctionID . '</p><p> Please go to http://localhost:8888/itemAuction.php?auctionID=' . $auctionID . ' to see your item</p>';
+        }
+     
+        //Create a new PHPMailer instance
+        $mail = new PHPMailer;
+        //Tell PHPMailer to use SMTP
+        $mail->isSMTP();
+        //Enable SMTP debugging
+        // 0 = off (for production use)
+        // 1 = client messages
+        // 2 = client and server messages
+        $mail->SMTPDebug = 2;
+        //Set the hostname of the mail server
+        $mail->Host = 'smtp.gmail.com';
+        // use
+        // $mail->Host = gethostbyname('smtp.gmail.com');
+        // if your network does not support SMTP over IPv6
+        //Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
+        $mail->Port = 587;
+        //Set the encryption system to use - ssl (deprecated) or tls
+        $mail->SMTPSecure = 'tls';
+        //Whether to use SMTP authentication
+        $mail->SMTPAuth = true;
+        //Username to use for SMTP authentication - use full email address for gmail
+        $mail->Username = "databasecoursework@gmail.com";
+        //Password to use for SMTP authentication
+        $mail->Password = "pFKdcJ4LxMNN8q7n";
+        //Set who the message is to be sent from
+        $mail->setFrom('databasecoursework@gmail.com', 'EbayLite');
+        //Set an alternative reply-to address
+        $mail->addReplyTo('databasecoursework@gmail.com', 'EbayLite');
+        //Set who the message is to be sent to
+        $mail->addAddress($email, $username);
+        //Set the subject line
+        $mail->Subject = $subj;
+        
+        //Read an HTML message body from an external file, convert referenced images to embedded,
+        //convert HTML into a basic plain-text alternative body
+        // $mail->msgHTML(file_get_contents('contents.html'), __DIR__);
+        $mail->msgHTML($msg);
+        //Replace the plain text body with one created manually
+        $mail->AltBody = 'This is a plain-text message body';
+        //Attach an image file
+        // $mail->addAttachment('images/phpmailer_mini.png');
+        //send the message, check for errors
+        ob_end_clean();
+        if (!$mail->send()) {
+            echo "Mailer Error: " . $mail->ErrorInfo;
+        } else {
+            echo "Message sent!";
+            //Section 2: IMAP
+            //Uncomment these to save your message in the 'Sent Mail' folder.
+            #if (save_mail($mail)) {
+            #    echo "Message saved!";
+            #}
+        }
+
+        $query = $this->dbconnection->prepare(
+            "INSERT INTO notifications (userID, auctionID, datetimeAdded) VALUES(?,?,NOW())"
+        );
+        $query->execute(array($userID, $auctionID));
+
+
+    }
 
     /**
      * Destroy the database connection when the object is no longer required
